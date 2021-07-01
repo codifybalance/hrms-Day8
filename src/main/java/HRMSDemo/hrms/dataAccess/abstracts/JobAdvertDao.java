538972a0d578a5d68f9648a133ab273a42a48bc6
Ayça Aydemir <40012179+codifybalance@users.noreply.github.com>
2021-07-01 05:12:13 +0300

    Update JobAdvertDao.java

--- src/main/java/HRMSDemo/hrms/dataAccess/abstracts/JobAdvertDao.java
+++ src/main/java/HRMSDemo/hrms/dataAccess/abstracts/JobAdvertDao.java
@@ -9,38 +9,14 @@ import HRMSDemo.hrms.entities.concretes.JobAdvert;
 public interface JobAdvertDao extends JpaRepository<JobAdvert,Integer>{
 	JobAdvert getByAdName(String adName); 
 	JobAdvert getByJobId(int jobId); 
-	/* JobAdvert getById(int id); */
 
-//	JobAdvert getByJobAdvertNameAndJobPosition_JobPositionId(String adName , String jobPositionId);
-	
-//	List<JobAdvert> getByJobAdvertAdNameOrJobPosition_JobPositionId(String adName , String jobPositionId);
-//	
-//	List<JobAdvert> getByJobPositionIn(List<Integer> job_positions);
-//
-//	List<JobAdvert> getByAdNameContains(String adName);
-//	
-//	List<JobAdvert> getByJobDescriptionStartsWith(String jobDescription);
-//	
-//	
-//	//JPQLnasılyazılır?
-	  @Query("From JobAdvert where adName=:adName")
-	  List<JobAdvert> getByAdName1(String adName);
+	@Query("From JobAdvert where adName=:adName")
+	List<JobAdvert> getByAdName1(String adName);
 
-		/* @Query("From JobAdvert where isActive=:isActive") */
-	  List<JobAdvert> findByIsActiveTrueOrderByApplicationDeadline();
+	List<JobAdvert> findByIsActiveTrueOrderByApplicationDeadline();
 	  
-	  //@Query("From JobAdvert where isActive=:isActive And employer.userId=:user_id")
-	  List<JobAdvert> findByIsActiveTrueAndEmployer_Id(int employerId);
-//		
-	  List<JobAdvert> findByIsActiveTrue();
-//
-	  //JobAdvert findByIdAndEmployer(int user_id);
-	  
-	  
-	  
-//	
-//	//select*from products where product_name=bisey and category_Id=bisey 
-//	//select*from products where category_id in(1,2,3,4)
 
-	
+	List<JobAdvert> findByIsActiveTrueAndEmployer_Id(int employerId);
+		
+	List<JobAdvert> findByIsActiveTrue();
 }
